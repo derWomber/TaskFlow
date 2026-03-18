5baf820fea98205cd52a498f31c75b47166ec7d7
derWomber <youssef.edu.2023@gmail.com>
2026-03-18 04:29:24 +0000

    darkmode, advanced settings added darkmode to the website and a div to add discription and priority to a task

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-let sidebar = document.getElementById('sidebar');
-let sidebarButtonOpen = document.getElementById('sidebarButtonOpen');
-let sidebarButtonClose = document.getElementById('sidebarButtonClose');
+function darkModus() {
+  document.body.classList.toggle("dark");
+    let darkModusButton = document.getElementById("theme");
+    if (darkModusButton) {
+        darkModusButton.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+    }
+}
 
-sidebarButtonOpen.addEventListener('click', () => {
-  sidebar.classList.toggle('show');
+let sidebar = document.getElementById("sidebar");
+let sidebarButtonOpen = document.getElementById("sidebarButtonOpen");
+let sidebarButtonClose = document.getElementById("sidebarButtonClose");
+let advancedSettings = document.getElementById("advancedSettings")
+let advancedSettingsButton = document.getElementById("advancedSettingsButton")
+
+
+sidebarButtonOpen.addEventListener("click", () => {
+  sidebar.classList.toggle("show");
 });
 
-sidebarButtonClose.addEventListener('click', () => {
-  sidebar.classList.remove('show');
+sidebarButtonClose.addEventListener("click", () => {
+  sidebar.classList.remove("show");
 });
+
+advancedSettingsButton.addEventListener("click", () => {
+  advancedSettings.classList.toggle("show");
+});
+
+
+
+
+
+function addTask() {
+    let input = document.getElementById("inputField").value;
+    console.log(input)
+}
